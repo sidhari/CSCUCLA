c341fc55580ee6d4518d97a452538d75239b88e7
William Nash <williamnash@Williams-MacBook-Pro-3.local>
2017-10-13 10:32:16 -0700

    implemented 12 bit sub-patterns

---
+++ CSCPatterns/include/PatternFitter.h
@@ -0,0 +1,15 @@
+/*
+ * PatternFitter.h
+ *
+ *  Created on: Oct 9, 2017
+ *      Author: williamnash
+ */
+
+#ifndef PATTERNFITTER_H_
+#define PATTERNFITTER_H_
+
+
+
+
+
+#endif /* PATTERNFITTER_H_ */
