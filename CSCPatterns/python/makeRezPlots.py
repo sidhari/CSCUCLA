3cd27ba1f82f633147a2fae8fece3e32c85d5816
Cameron Bravo <c.bravo@cern.ch>
2017-12-14 16:31:10 -0600

    Adding features to CSCPatterns plotting tool

--- CSCPatterns/python/makeRezPlots.py
+++ CSCPatterns/python/makeRezPlots.py
@@ -2,7 +2,7 @@
 import ROOT as r
 
 #open file
-inF = r.TFile("../src/plotTree.root")
+inF = r.TFile("/home/bravo/move/plotTree.root")
 myT = inF.plotTree
 
 #init plots
@@ -14,6 +14,8 @@ Nproc = 0
 for event in myT:
     Nproc += 1
     if Nproc%1000 == 0: print "Processing event %i of %i"%(Nproc,Nev)
+    if event.RI != 1 or event.ST != 2: continue
+    #if event.segmentX < 10.0 or event.segmentX > 70.0: continue
     if not (event.envelopeId in rezPlots): rezPlots[event.envelopeId] = {}
     if not (event.patternId in rezPlots[event.envelopeId]): rezPlots[event.envelopeId][event.patternId] = r.TH1D(
             "rez%i_%i_h"%(event.envelopeId,event.patternId),
@@ -30,6 +32,8 @@ Nproc = 0
 for event in myT:
     Nproc += 1
     if Nproc%1000 == 0: print "Processing event %i of %i"%(Nproc,Nev)
+    if event.RI != 1 or event.ST != 2: continue
+    #if event.segmentX < 10.0 or event.segmentX > 70.0: continue
     posRes_h.Fill(event.segmentX-event.patX-rezPlots[event.envelopeId][event.patternId].GetMean())
     envPosRes_h.Fill(event.segmentX-event.patX)
     pass
