dfced8749374f932f56f267e0670a8d1d3a77f90
Cameron Bravo <c.bravo@cern.ch>
2017-12-14 19:40:24 -0600

    Update plotter to use ave file

--- CSCPatterns/python/makeRezPlots.py
+++ CSCPatterns/python/makeRezPlots.py
@@ -8,33 +8,32 @@ myT = inF.plotTree
 #init plots
 rezPlots = {}
 
-#loop over tree
-Nev = myT.GetEntries()
-Nproc = 0
-for event in myT:
-    Nproc += 1
-    if Nproc%1000 == 0: print "Processing event %i of %i"%(Nproc,Nev)
-    if event.RI != 1 or event.ST != 2: continue
-    #if event.segmentX < 10.0 or event.segmentX > 70.0: continue
-    if not (event.envelopeId in rezPlots): rezPlots[event.envelopeId] = {}
-    if not (event.patternId in rezPlots[event.envelopeId]): rezPlots[event.envelopeId][event.patternId] = r.TH1D(
-            "rez%i_%i_h"%(event.envelopeId,event.patternId),
-            "rez%i_%i_h;position difference [strip];Number of Segments"%(event.envelopeId,event.patternId),
-            600,-3.0,3.0)
-    rezPlots[event.envelopeId][event.patternId].Fill(event.segmentX-event.patX)
+#Build full ID lookup
+idT = r.TTree("idTree","Tree holding preproccessed id info")
+idT.ReadFile("CSCDigiTree161031.ave")
+idMean = {}
+idNseg = {}
+for event in idT:
+    idMean[event.patID] = event.mean
+    idNseg[event.patID] = event.Nseg
     pass
 
 #draw stuff
 posRes_h = r.TH1D("posRes_h","posRes_h",400,-2.0,2.0)
 envPosRes_h = r.TH1D("envPosRes_h","envPosRes_h",400,-2.0,2.0)
 outF = r.TFile("rezPlots.root","RECREATE")
+Nev = myT.GetEntries()
 Nproc = 0
 for event in myT:
     Nproc += 1
     if Nproc%1000 == 0: print "Processing event %i of %i"%(Nproc,Nev)
     if event.RI != 1 or event.ST != 2: continue
+    if not (event.patternId in idMean): 
+        print "patId %i does not exist in ave file"%event.patternId
+        continue
+    if idNseg[event.patternId] < 10: continue
     #if event.segmentX < 10.0 or event.segmentX > 70.0: continue
-    posRes_h.Fill(event.segmentX-event.patX-rezPlots[event.envelopeId][event.patternId].GetMean())
+    posRes_h.Fill(event.segmentX-event.patX-idMean[event.patternId])
     envPosRes_h.Fill(event.segmentX-event.patX)
     pass
 
