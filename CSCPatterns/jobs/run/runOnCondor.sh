6e1ebdf5bccfd0c88335ac7cd45b768c24f8595b
Will Nash <wnash@bu.edu>
2018-06-14 18:02:28 -0700

    Adding jobs directory

---
+++ CSCPatterns/jobs/run/runOnCondor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CMSVERS=CMSSW_9_2_8
+
+#sets up environment so c++ executable knows where libraries are
+echo "Starting job on " `date` #Date/time of start of job
+echo "Running on: `uname -a`" #Condor job is running on this node
+echo "System software: `cat /etc/redhat-release`" #Operating System on that node
+source /cvmfs/cms.cern.ch/cmsset_default.sh  ## if a tcsh script, use .csh instead of .sh
+export SCRAM_ARCH=slc6_amd64_gcc530
+eval `scramv1 project CMSSW ${CMSVERS}`
+cp {${1},${2}} $CMSVERS/src/
+cd $CMSVERS/src/
+eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
+
+#executable, inputfile, outputfile
+./${1} ${2} ${3}
+mv ${3} ../../ #move out of CMSSW build so it transfers properly
