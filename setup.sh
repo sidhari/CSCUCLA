2c89c75efc9c0a225763086c3f05fb85b2b6f276
Chau Dao <chaunbdao@g.ucla.edu>
2019-08-02 14:03:09 -0700

    fixed setup.sh to do symlinks correctly

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-ln -s CSCDigiTuples/include/CSCHelper.h CSCPatterns/include/CSCHelper.h
-ln -s CSCDigiTuples/include/CSCInfo.h CSCPatterns/include/CSCInfo.h
+base=$(pwd)
+subdir1="/CSCDigiTuples/include/CSCHelper.h"
+subdir2="/CSCPatterns/include"
+subdir3="/CSCDigiTuples/include/CSCInfo.h"
+subdir4="/CSCPatterns/include"
+newpath1=$base$subdir1
+newpath2=$base$subdir2
+newpath3=$base$subdir3
+newpath4=$base$subdir4
+ln -s $newpath1 $newpath2
+ln -s $newpath3 $newpath4
