ee9a205daadabd8a40e8e1e52d37b64b639101a7
Chau Dao <chaunbdao@g.ucla.edu>
2019-08-06 16:31:02 -0700

    added chamber printer and modded setup.sh

---
+++ CSCPatterns/include/ALCTChamberPrinter.h
@@ -0,0 +1,29 @@
+/*
+ * ALCTChamberPrinter.h
+ *
+ *  Created on: 06 August 2019
+ *      Author: Chau Dao
+ */
+
+#ifndef CSCPATTERNS_INCLUDEs_ALCTChamberPrinter_H_
+#define CSCPATTERNS_INCLUDE_ALCTChamberPrinter_H_
+
+#include "../include/Processor.h"
+
+class ALCTChamberPrinter
+{
+    public:
+        AlCTChamberPrinter(){};
+        int run(string inputfile, unsigned int EC, unsigned int ST, unsigned int RI, unsigned int CH);
+        int main(int argc, char* argv[])
+        {
+            std::string s(argv[1]);
+            ALCTChamberPrinter::run(s,  (unsigned int) atoi(argv[2]),
+                                        (unsigned int) atoi(argv[3]),
+                                        (unsigned int) atoi(argv[4]),
+                                        (unsigned int) atoi(argv[5]))
+        }
+}
+
+
+#endif
